e6ee1dcbc273b77663bc230d777e277ac602aa71
Isaac Kao <hsinlun@google.com>
2024-08-09 15:18:57 +0800

    Use unittest to implement the FT

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewvisitorTest(unittest.TestCase):
 
-assert 'install' in browser.title
+  def setUp(self):
+    self.browser = webdriver.Chrome()
+    self.browser.implicitly_wait(3)
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_can_start_a_list_and_retrieve_it_later(self):
+    self.browser.get('http://localhost:8000')
+    self.assertIn('To-Do', self.browser.title)
+    self.fail('Finish the test!')
+    #self.fail(f'Browser title was \"{browser.title}\"')
+
+# assert 'To-Do' in browser.title, f'Browser title was \"{browser.title}\"'
+if __name__ == '__main__':
+  unittest.main(warnings='ignore')
